13d93d71a97b6fd5cac8397768e8da0721c1370d
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2022-04-18 00:53:06 -0300

    Cambios en los estilos de app y listCharacters.

--- rick-and-morty-api/src/App.vue
+++ rick-and-morty-api/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <!--<img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <div>
-    <h1>Rick and Morty</h1>
-    <list-characters/>
+  <div class="container">
+    <h1>Rick And Morty</h1>
+    <FilterByStatus />
+    <FilterByName />
+    <ListCharacters />
   </div>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
 import ListCharacters from '@/components/ListCharacters'
+import FilterByStatus from '@/components/FilterByStatus'
+import FilterByName from '@/components/FilterByName'
 export default {
   name: 'App',
   components: {
-    //HelloWorld
-    ListCharacters
+    ListCharacters,
+    FilterByStatus,
+    FilterByName
   }
 }
 </script>
@@ -29,4 +34,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }*/
+:root {
+  --background-body: #24282F;
+  --background-card: #3C3E44;
+  --text-white: #FFFFFF;
+  --text-gray: #c0c0c0;
+  --text-orange: #FF9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
+  text-align: center;
+}
+.container {
+  width: 980px;
+  max-width: 90%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
+}
 </style>
