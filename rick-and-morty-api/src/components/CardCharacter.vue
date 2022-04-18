e66f048a2709dc9f0379eb15267ad3e5c924bb38
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2022-04-18 00:44:23 -0300

    componentes CardCharacter y FilterByName creados y subidos.

---
+++ rick-and-morty-api/src/components/CardCharacter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="character">
+    <img :src="character.image" :alt="character.name" />
+    <div class="character__info">
+      <h3>{{ character.name }}</h3>
+      <div class="status">
+          <span
+            :class="
+                character.status == 'Alive' ? 'alive' :
+                character.status == 'Dead' ? 'dead' :
+                'default'
+            "
+          ></span>
+          <span>{{character.status}} - {{character.species}}</span>
+      </div>
+      <div class="origin">
+          <span>
+              Origin:
+              {{character.origin.name}}
+          </span>
+      </div>
+      <div class="location">
+          Location: 
+          {{character.location.name}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["character"],
+};
+</script>
+
+<style>
+</style>
