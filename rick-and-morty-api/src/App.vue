90c61be94cb62a0f747895503d98f70890ace7ba
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2022-04-17 22:10:44 -0300

    Proyecto base  Rick and Morty generado con Vue CLI.

---
+++ rick-and-morty-api/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--<img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div>
+    <h1>Rick and Morty</h1>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+/*
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
